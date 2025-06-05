3924bbff6ec9b4efdca173d1af899acf23dcb372
Ronaldium23 <ronaldium23@gmail.com>
2025-06-05 01:16:31 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -49,4 +49,6 @@ def send_email():
     return '', 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
